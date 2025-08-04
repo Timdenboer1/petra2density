b0ba49d0854c2a0c66f1ac47148761e89acedeb0
Timdenboer1 <tim.denboer@psych.ox.ac.uk>
2025-08-04 11:29:45 +0100

    Testing

--- petra2density.sh
+++ petra2density.sh
@@ -7,9 +7,8 @@ if [[ $# -lt 3 ]]; then
     exit 2
 fi
 
-
-
-PETRA2DENSITY_DIR="$(dirname "$(which "$0")")"
+PETRA2DENSITY_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo "PETRA2DENSITY_DIR: $PETRA2DENSITY_DIR"
 REGISTER_TO_PETRA=0
 
 while getopts "h:p" opt; do
@@ -24,12 +23,51 @@ done
 
 shift $((OPTIND - 1))
 
-if [ "$REGISTER_TO_PETRA" -eq 1 ]; then
-    charm "$1" "$3" "$2" --usesettings $PETRA2DENSITY_DIR/charm.ini --forceqform
-    python $PETRA2DENSITY_DIR/petra2density.py "./m2m_$1" --register_to_petra
-    exit
-fi
+SUBJECT_ID="tim"
+DATA_DIR="/Users/timdenboer1/local_drive_data/AmygTUS_Plasticity-Techdev/F3T_2024_023_050"
+PETRA_FILE="${DATA_DIR}/images_031_noT1_petra_tra_FA_1_deg.nii"
 
-charm "$1" "$2" "$3" --usesettings $PETRA2DENSITY_DIR/charm.ini --forceqform
-python $PETRA2DENSITY_DIR/petra2density.py "./m2m_$1"
+T1_IMAGES=(
+  "images_04_t1_mprage_sag_p2_iso.nii"
+ #"images_026_t1_mprage_sag_p2_iso_FSWEn.nii"
+  #"images_028_t1_mprage_sag_p2_isoFSWEf.nii"
+)
 
+for T1_IMAGE in "${T1_IMAGES[@]}"; do
+  # Extract the string at the end of the T1 image filename before .nii (e.g., FSWEn, FSWEf, classic)
+  T1_ABBR=$(echo "$T1_IMAGE" | sed -E 's/.*_([A-Za-z0-9]+)\.nii/\1/')
+  if [ "$T1_ABBR" = "iso" ]; then
+    T1_ABBR="classic"
+  fi
+  SUBJECT_ID="tim_${T1_ABBR}"
+  T1_PATH="${DATA_DIR}/${T1_IMAGE}"
+
+  # Set output directory based on T1_ABBR
+  case "$T1_ABBR" in
+    FSWEn)
+      OUT_DIR="${DATA_DIR}/T1/T1_fatSupr_normal"
+      ;;
+    isoFSWEf)
+      OUT_DIR="${DATA_DIR}/T1/T1_fatSupr_fast"
+      ;;
+    classic)
+      OUT_DIR="${DATA_DIR}/T1/T1_classic"
+      ;;
+    *)
+      OUT_DIR="${DATA_DIR}"
+      ;;
+  esac
+
+  if [ "$REGISTER_TO_PETRA" -eq 1 ]; then
+      cd "$OUT_DIR"
+      echo "$OUT_DIR"
+      charm "$SUBJECT_ID" "$PETRA_FILE" "$T1_PATH" --usesettings $PETRA2DENSITY_DIR/charm.ini --forceqform
+      python3 $PETRA2DENSITY_DIR/petra2density.py "$OUT_DIR/m2m_${SUBJECT_ID}" --register_to_petra
+      cd "$PETRA2DENSITY_DIR"
+  else
+      cd "$OUT_DIR"
+      charm "$SUBJECT_ID" "$T1_PATH" "$PETRA_FILE" --usesettings $PETRA2DENSITY_DIR/charm.ini --forceqform
+      python3 $PETRA2DENSITY_DIR/petra2density.py "$OUT_DIR/m2m_${SUBJECT_ID}"
+      cd "$PETRA2DENSITY_DIR"
+  fi
+done
